35f02658e95b4695df8b35c35d0558979217a661
Askartius <151680098+askartius@users.noreply.github.com>
2023-12-02 23:56:49 +0300

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int A = scanner.nextInt();
+        int quotient = 0;
+
+        while(A >= 2) {
+            A /= 2;
+            quotient++;
+        }
+
+        System.out.println(quotient + 1);
     }
 }
