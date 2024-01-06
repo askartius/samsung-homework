a35f3d5386db3bfc709c441b9910548c9e161a65
Askartius <151680098+askartius@users.noreply.github.com>
2024-01-06 10:33:02 +0300

    Additional task 1

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+        int k = scanner.nextInt();
+        int n = 1;
 
-        int num = scanner.nextInt() + 1;
-        boolean isPrime = false;
-
-        while (!isPrime) {
-            for (int primeNumber : primeNumbers) {
-                if (num % primeNumber != 0) {
-                    isPrime = true;
-                } else {
-                    if (num == primeNumber) {
-                        isPrime = true;
-                    } else {
-                        isPrime = false;
-                        num += 1;
-                    }
-                    break;
-                }
+        while (n <= k) {
+            if (scanner.nextInt() <= 437) {
+                System.out.println("Crash " + n);
+                break;
             }
+            n += 1;
         }
 
-        System.out.println(num);
+        if (n > k) {
+            System.out.println("No crash");
+        }
     }
 }
