b28da15d20124d806ba331d4c46ab85d283d2fa1
Askartius <151680098+askartius@users.noreply.github.com>
2024-01-07 23:52:22 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int count = scanner.nextInt();
-        int sum = 0;
-        boolean noEven = true;
+        int oddSum = 0;
+        int oddCount = 0;
 
         for (int i = 0; i < count; i++) {
             int num = scanner.nextInt();
-            if (num % 2 == 0) {
-                sum += num;
-                noEven = false;
+            if (num % 2 != 0) {
+                oddSum += num;
+                oddCount++;
             }
         }
 
-        System.out.println(noEven ? "NO" : sum);
+        if (oddCount == 0) {
+            System.out.println("NO");
+        } else {
+            System.out.printf("%.2f", (float) oddSum / oddCount);
+        }
     }
 }
